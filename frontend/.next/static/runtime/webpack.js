81d38b39ee91514e9f4dc75f1d608bc0fe45bcd2
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-01-09 10:39:55 -0800

    added link

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "19e1a9b85a5a0091190f";
+/******/ 	var hotCurrentHash = "606141eed1be1f1e0e9e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
