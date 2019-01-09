4fd666c50f630f5bd1673c816d2c9f08102bb19d
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-01-09 14:39:35 -0800

    styled header

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -6,7 +6,7 @@ const theme = {
   red: "#FF0000",
   black: "#393939",
   grey: "#3A3A3A",
-  lightgrey: "E1E1E1",
+  lightGrey: "E1E1E1",
   offWhite: "EDEDED",
   maxWidth: "1000px",
   bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
@@ -14,11 +14,11 @@ const theme = {
 
 const StyledPage = styled.div`
   background: white;
-  color: black;
+  color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
-  max-width: 1000px;
+  max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
 `;
