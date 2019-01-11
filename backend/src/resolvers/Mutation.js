7b8ab25f40baf6e709033e1a8761ab033ef4822c
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-01-10 18:47:01 -0800

    updated datamodel

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    //create dog
+    const newDog = { name: args.name };
+    console.log(newDog);
+    global.dogs.push(newDog);
+    return newDog;
+  }
+};
 
 module.exports = Mutations;
