fda7bd6e465f390fc5ab5f24de29629b1cbe6a47
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2019-01-10 20:05:52 -0800

    added custom resolvers

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,12 +1,25 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    //create dog
-    const newDog = { name: args.name };
-    console.log(newDog);
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    //TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
   }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   //create dog
+  //   const newDog = { name: args.name };
+  //   console.log(newDog);
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
 };
 
 module.exports = Mutations;
